293ee1b2ec490d8e3036ac972e7c357ab4ad5693
RiteshGithub04 <85405340+RiteshGithub04@users.noreply.github.com>
2021-06-06 16:31:45 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h4>Javascript and React.js</h4>
+      <p>
+        7Days Bootcamp on Javascript and React.js at 7.30pm from 25may onwards
+        Shauyra sinha will be mentor
+      </p>
+    </div>
+  );
+}
+export default Note;
